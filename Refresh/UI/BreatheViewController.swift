9ce3fe9f7abd175bee481eb9f3f8d9f0ebce659c
Austin Louden <austinlouden@gmail.com>
2020-08-18 21:35:12 -0700

    Hide status bar, add welcome label, disable landscape

--- Refresh/UI/BreatheViewController.swift
+++ Refresh/UI/BreatheViewController.swift
@@ -11,9 +11,10 @@ import UIKit
 class BreatheViewController: UIViewController {
 
     var constraints = [NSLayoutConstraint]()
+    let welcomeLabel = UILabel()
     let startButton = UIButton(type: .roundedRect)
-    
-    // UI Constants
+
+    // Constants
     let viewMargin: CGFloat = 20
     let buttonHeight: CGFloat = 44
     let buttonCornerRadius: CGFloat = 8
@@ -21,7 +22,9 @@ class BreatheViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.systemBackground
-        
+        title = NSLocalizedString("Welcome.", comment: "The title displayed on the primary screen.")
+
+        navigationController?.navigationBar.prefersLargeTitles = true
         let settingsButton = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(self.settingsButtonPressed))
         navigationItem.setRightBarButton(settingsButton, animated: false)
     
@@ -33,6 +36,13 @@ class BreatheViewController: UIViewController {
         startButton.layer.cornerRadius = buttonCornerRadius
         startButton.layer.backgroundColor = CGColor(srgbRed: 99.0/255.0, green: 209.0/255.0, blue: 159.0/255.0, alpha: 1.0)
         view.addSubview(startButton)
+        
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        welcomeLabel.numberOfLines = 0
+        welcomeLabel.textColor = .secondaryLabel
+        welcomeLabel.text = NSLocalizedString("Take a few seconds to find a comfortable spot.",
+                                              comment: "Tells the user to a comfortable position before starting the exercise.")
+        view.addSubview(welcomeLabel)
     }
     
     override func updateViewConstraints() {
@@ -42,6 +52,11 @@ class BreatheViewController: UIViewController {
             constraints.append(startButton.trailingAnchor.constraint(equalTo: layoutGuide.trailingAnchor, constant: -viewMargin))
             constraints.append(startButton.heightAnchor.constraint(equalToConstant: buttonHeight))
             constraints.append(startButton.bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor, constant: -viewMargin))
+            
+            constraints.append(welcomeLabel.topAnchor.constraint(equalTo: layoutGuide.topAnchor))
+            constraints.append(welcomeLabel.leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor, constant: viewMargin))
+            constraints.append(welcomeLabel.trailingAnchor.constraint(equalTo: layoutGuide.trailingAnchor, constant: -viewMargin))
+
             NSLayoutConstraint.activate(constraints)
         }
         super.updateViewConstraints()
